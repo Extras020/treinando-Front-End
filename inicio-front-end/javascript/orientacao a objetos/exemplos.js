26541ad98905b39b130afcb52511378317a6f31d
labinfo4 <labinfo4@ectufrn.ect.ufrn.br>
2026-03-23 19:53:55 -0300

    progresso 2

---
+++ inicio-front-end/javascript/orientacao a objetos/exemplos.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")()
+let aluno = {
+    nome: "esdras",
+    idade: 23,
+    curso: "Ciencias e tecnologia"
+}
+console.log(aluno.nome)
+console.log(aluno.idade)
+console.log(aluno.curso)

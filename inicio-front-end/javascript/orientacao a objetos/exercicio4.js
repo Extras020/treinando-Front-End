20db8ca9af5750cf5d25308a0ceb5d881ecb265f
labinfo4 <labinfo4@ectufrn.ect.ufrn.br>
2026-03-27 17:18:18 -0300

    evoluindo

--- inicio-front-end/javascript/orientacao a objetos/exercicio4.js
+++ inicio-front-end/javascript/orientacao a objetos/exercicio4.js
@@ -10,9 +10,20 @@ class contaBancaria{
     sacar(valor){
         this.saldo = this.saldo - valor
     }
-    transferir(titular, valor){
+    transferir(contaDestino, valor){
+        contaDestino.saldo = contaDestino.saldo + valor
         this.saldo = this.saldo - valor
-        
+        console.log("Transferência realizada!")
     }
 }
-/// ainda vou terminar
+let titular = prompt("Digite o nome do titular da conta 1: ")
+let saldo = Number(prompt("Digite o saldo na conta do titular da conta 1: "))
+let conta1 = new contaBancaria(titular, saldo)
+titular = prompt("Digite o nome do titular da conta 2: ")
+saldo = Number(prompt("Digite o saldo na conta do titular da conta 2: "))
+let conta2 = new contaBancaria(titular, saldo)
+
+let valor = Number(prompt("digite um valor para transferir da conta 1 para a conta 2: "))
+conta1.transferir(conta2, valor)
+console.log(`saldo na conta 1: ${conta1.saldo}`)
+console.log(`saldo na conta 2: ${conta2.saldo}`)

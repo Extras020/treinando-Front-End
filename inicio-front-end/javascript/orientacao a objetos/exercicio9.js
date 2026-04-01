446e93420b6df5afcfab50f02a0c68c7de8facc7
labinfo4 <labinfo4@ectufrn.ect.ufrn.br>
2026-04-01 16:53:19 -0300

    mais duas

--- inicio-front-end/javascript/orientacao a objetos/exercicio9.js
+++ inicio-front-end/javascript/orientacao a objetos/exercicio9.js
@@ -5,22 +5,30 @@ class Celular{
         this.#bateria = 100
     }
     usar(appTempo){
-        if(this.#bateria - appTempo < 0){
+        let carga_utilizada = (appTempo/100) * this.#bateria
+        if(this.#bateria - carga_utilizada < 0){
             console.log("não há carga disponivel!")
         }
         else{
-            this.#bateria = this.#bateria - appTempo
+            this.#bateria = this.#bateria - carga_utilizada
         }
     }
     carregar(valor){
-        if(this.#bateria + valor > 0){
+        if(this.#bateria + valor > 100){
             console.log("sobrecarga de bateria!")
         }
         else{
-            this.#bateria + valor
+            this.#bateria = this.#bateria + valor
         }
     }
     mostrarBateria(){
         console.log(`bateria: ${this.#bateria}%`)
     }
 }
+let c1 = new Celular()
+let valor = Number(prompt("digite um tempo de uso da bateria: "))
+c1.usar(valor)
+c1.mostrarBateria()
+valor = Number(prompt("digite um valor para recarregar a bateria: "))
+c1.carregar(valor)
+c1.mostrarBateria()

c3499c51047de6c2d837f1764c1018102fa2ba10
labinfo4 <labinfo4@ectufrn.ect.ufrn.br>
2026-04-10 17:40:45 -0300

    rascunho

--- inicio-front-end/javascript/orientacao a objetos/sistema de biblioteca/types.js
+++ inicio-front-end/javascript/orientacao a objetos/sistema de biblioteca/types.js
@@ -1,3 +1,17 @@
+class Biblioteca{
+    constructor(){
+        this.livros = []
+        this.usuarios = []
+    }
+    adicionarLivro(id){
+        let cont = 0
+        for(let i = 0; i < length.livros; i++){
+            if(id == this.livros[i].retornaId){
+                cont++
+            }
+        }
+    }
+}
 class Livro{
     #id
     #qnt
@@ -14,6 +28,9 @@ class Livro{
         console.log(`Quantidade: ${this.#qnt}`)
         console.log(`Identificador: ${this.#id}`)
     }
+    retornaId(){
+        return this.#id
+    }
 }
 class Usuario{
     #cpf
@@ -27,6 +44,11 @@ class Usuario{
         console.log(`Nome: ${this.nome}`)
         console.log(`Idade: ${this.idade}`)
         console.log(`CPF: ${this.#cpf}`)
+        console.log("Emprestimos:")
+        for(let i = 0; i < length.emprestados; i++){
+            console.log(`Nome: ${this.emprestados[i].mostrarDados()}`)
+            console.log("------------------------------------------")
+        }
     }
 }
 class Data_Publi{

6c289d0824659b03103cca50ae017e196ed6171d
labinfo4 <labinfo4@ectufrn.ect.ufrn.br>
2026-04-30 18:05:44 -0300

    menu

--- inicio-front-end/javascript/orientacao a objetos/sistema de biblioteca/main.js
+++ inicio-front-end/javascript/orientacao a objetos/sistema de biblioteca/main.js
@@ -5,21 +5,59 @@ import { criarLivro, criarUsuario, requisicao_emprestimo, solicita_devolucao } f
 import { sistema } from './sistema.js'
 
 while(true){
+    console.log("1 - Cadastrar livro")
+    console.log("2 - Cadastrar usuário")
+    console.log("3 - Buscar usuário")
+    console.log("4 - Buscar Livro")
+    console.log("5 - Empréstimo")
+    console.log("6 - Devolução")
+    console.log("7 - Sair")
     let escolha = Number(prompt())
     if(escolha == 1){
         let livro = criarLivro()
-        sistema.cadastrarLivro(livro)
+        if(livro != undefined){
+            sistema.cadastrarLivro(livro)
+        }
+        else{
+            console.log("algo deu errado, tente novamente!")
+        }
     }
-    if(escolha == 2){
+    else if(escolha == 2){
         let usuario = criarUsuario()
-        sistema.cadastrarUsuario(usuario)
+        if(usuario != undefined){
+            sistema.cadastrarUsuario(usuario)
+        }
+        else{
+            console.log("algo deu errado, tente novamente!")
+        }
     }
-    if(escolha == 3){
-        let cpf = prompt()
+    else if(escolha == 3){
+        let cpf = prompt("Digite o CPF do usuário: ")
         sistema.buscarUsuario(cpf)
     }
-    if(escolha == 4){
-        let id = prompt()
+    else if(escolha == 4){
+        let id = prompt("Digite o Id do livro: ")
         sistema.buscarLivro(id)
+    }
+    else if(escolha == 5){
+        let req_emprestimo = requisicao_emprestimo()
+        if(req_emprestimo != undefined){
+            sistema.emprestarLivro(req_emprestimo)
+        }
+        else{
+            console.log("algo deu errado, tente novamente!")
+        }
+    }
+    else if(escolha == 6){
+        let sol_devolucao = solicita_devolucao()
+        if(sol_devolucao != undefined){
+            sistema.devolverLivro(sol_devolucao)
+        }
+        else{
+            console.log("algo deu errado, tente novamente")
+        }
+    }
+    else if(escolha == 7){
+        break
     }
 }

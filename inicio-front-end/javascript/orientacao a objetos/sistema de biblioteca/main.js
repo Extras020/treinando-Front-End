e638889b0de12fb9a9b430174ea5e45479c38602
labinfo4 <labinfo4@ectufrn.ect.ufrn.br>
2026-05-05 19:16:51 -0300

    organizando o menu

--- inicio-front-end/javascript/orientacao a objetos/sistema de biblioteca/main.js
+++ inicio-front-end/javascript/orientacao a objetos/sistema de biblioteca/main.js
@@ -3,61 +3,24 @@ const prompt = promptSync()
 import { Livro, Usuario, Biblioteca } from './classes.js'
 import { criarLivro, criarUsuario, requisicao_emprestimo, solicita_devolucao } from './objetos.js'
 import { sistema } from './sistema.js'
+import { menuLivro, menuUsuario, menuEmprestimos } from './menus.js'
 
 while(true){
-    console.log("1 - Cadastrar livro")
-    console.log("2 - Cadastrar usuário")
-    console.log("3 - Buscar usuário")
-    console.log("4 - Buscar Livro")
-    console.log("5 - Empréstimo")
-    console.log("6 - Devolução")
-    console.log("7 - Sair")
+    console.log("1 - Livros")
+    console.log("2 - Usuários")
+    console.log("3 - Empréstimos")
+    console.log("0 - Sair")
     let escolha = Number(prompt())
     if(escolha == 1){
-        let livro = criarLivro()
-        if(livro != undefined){
-            sistema.cadastrarLivro(livro)
-        }
-        else{
-            console.log("algo deu errado, tente novamente!")
-        }
+        menuLivro()
     }
     else if(escolha == 2){
-        let usuario = criarUsuario()
-        if(usuario != undefined){
-            sistema.cadastrarUsuario(usuario)
-        }
-        else{
-            console.log("algo deu errado, tente novamente!")
-        }
+        menuUsuario()
     }
     else if(escolha == 3){
-        let cpf = prompt("Digite o CPF do usuário: ")
-        sistema.buscarUsuario(cpf)
+        menuEmprestimos()
     }
-    else if(escolha == 4){
-        let id = prompt("Digite o Id do livro: ")
-        sistema.buscarLivro(id)
-    }
-    else if(escolha == 5){
-        let req_emprestimo = requisicao_emprestimo()
-        if(req_emprestimo != undefined){
-            sistema.emprestarLivro(req_emprestimo)
-        }
-        else{
-            console.log("algo deu errado, tente novamente!")
-        }
-    }
-    else if(escolha == 6){
-        let sol_devolucao = solicita_devolucao()
-        if(sol_devolucao != undefined){
-            sistema.devolverLivro(sol_devolucao)
-        }
-        else{
-            console.log("algo deu errado, tente novamente")
-        }
-    }
-    else if(escolha == 7){
+    else if(escolha == 0){
         break
     }
 }
---
+++ inicio-front-end/javascript/orientacao a objetos/sistema de biblioteca/menus.js
@@ -0,0 +1,92 @@
+import promptSync from 'prompt-sync'
+const prompt = promptSync()
+import { Livro, Usuario, Biblioteca } from './classes.js'
+import { criarLivro, criarUsuario, requisicao_emprestimo, solicita_devolucao } from './objetos.js'
+import { sistema } from './sistema.js'
+
+
+export function menuLivro(){
+    while(true){
+        console.log("1 - Cadastrar Livro")
+        console.log("2 - Buscar Livro")
+        console.log("3 - Listar acervo")
+        console.log("0 - Voltar")
+        let escolha = prompt()
+        if(escolha == 1){
+            let livro = criarLivro()
+            if(livro != undefined){
+                sistema.cadastrarLivro(livro)
+            }
+            else{
+                console.log("algo deu errado, tente novamente!")
+            }
+        }
+        else if(escolha == 2){
+            let id = prompt("Digite o Id do livro: ")
+            sistema.buscarLivro(id)
+        }
+        else if(escolha == 3){
+            sistema.listarAcervo()
+        }
+        else if(escolha == 0){
+            return
+        }
+    }
+}
+export function menuUsuario(){
+    while(true){
+        console.log("1 - Cadastrar Usuário")
+        console.log("2 - Buscar Usuário")
+        console.log("3 - Listar Usuários")
+        console.log("0 - Voltar")
+        let escolha = prompt()
+        if(escolha == 1){
+            let usuario = criarUsuario()
+            if(usuario != undefined){
+                sistema.cadastrarUsuario(usuario)
+            }
+            else{
+                console.log("algo deu errado, tente novamente!")
+            }
+        }
+        else if(escolha == 2){
+            let cpf = prompt("Digite o CPF do usuário: ")
+            sistema.buscarUsuario(cpf)
+        }
+        else if(escolha == 3){
+            sistema.listarUsuarios()
+        }
+        else if(escolha == 0){
+            return
+        }
+    }
+}
+export function menuEmprestimos(){
+    while(true){
+        console.log("1 - Emprestar Livro")
+        console.log("2 - Devolver Livro")
+        console.log("0 - Voltar")
+        let escolha = prompt()
+        if(escolha == 1){
+            let req_emprestimo = requisicao_emprestimo()
+            if(req_emprestimo != undefined){
+                sistema.emprestarLivro(req_emprestimo)
+            }
+            else{
+                console.log("algo deu errado, tente novamente!")
+            }
+        }
+        else if(escolha == 2){
+            let sol_devolucao = solicita_devolucao()
+            if(sol_devolucao != undefined){
+                sistema.devolverLivro(sol_devolucao)
+            }
+            else{
+                console.log("algo deu errado, tente novamente")
+            }
+        }
+        else if(escolha == 0){
+            return
+        }
+    }
+}
